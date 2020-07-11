00f7068b9653c4c4441471c9afd98534bf8e5321
muhhizbe <abumukhtar88@gmail.com>
2020-07-12 06:33:31 +0700

    create template auth

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  @yield('title')
+
+  @include('layouts.style.main')
+</head>
+
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Begin Page Content -->
+        @yield('content')
+        <!-- End of Main Content -->
+      </div>
+      <!-- Footer -->
+      @include('layouts.components.footer')
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  @include('layouts.components.modal')
+
+  @include('layouts.scripts.main')
+
+  <!-- script tambahan sweet alert, bukan dari bawaan sb-admin-2 -->
+  @stack('additional_scripts')
+
+
+
+</body>
+
+</html>
