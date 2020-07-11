88c7c684e1106168ea02be96fb4a00ecd68aba27
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-07-11 10:52:02 +0700

    isolate tinymce scripts to avoid repetition

---
+++ resources/views/layouts/scripts/tinymce.blade.php
@@ -0,0 +1,18 @@
+<script src="https://cdn.tiny.cloud/1/urbhmkislnnqn2038vdfqu9w384er53gobfi9y1ear1ql6er/tinymce/5/tinymce.min.js" referrerpolicy="origin"/></script>
+<script type="text/javascript">
+    tinymce.init({
+        selector: 'textarea.tinymce-editor',
+        height: 500,
+        menubar: false,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+        content_css: '//www.tiny.cloud/css/codepen.min.css'
+    });
+</script>
