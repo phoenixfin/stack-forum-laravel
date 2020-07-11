fb57ff8dfdb71af6854b8b12d0fb7a505b630f99
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-07-11 10:51:25 +0700

    update model for answer

--- app/Models/AnswerModel.php
+++ app/Models/AnswerModel.php
@@ -22,8 +22,30 @@ class AnswerModel {
       return $answers;
     }
 
+    public static function find_by_id($id) {
+        $answer = DB::table('answers')->where('id', $id)->first();
+        return $answer;        
+    }
+
     public static function insert($data) {
-        $new_answer = DB::table('answer')->insert($data);
+        $new_answer = DB::table('answers')->insert($data);
         return $new_answer;
     }
+    
+    public static function update($id, $request) {
+        $answer = DB::table('answers')
+                      ->where('id',$id)
+                      ->update([
+                        'content'=>$request['content']
+                      ]);
+        return $answer;
+    }    
+    
+    public static function destroy($id) {
+        $del_ans = DB::table('answers')
+                     -> where('id', $id)
+                     -> delete();
+        return $del_ans;
+    }
+
 }
