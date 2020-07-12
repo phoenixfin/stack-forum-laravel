56e31e3f57a0c7a059f6f34b9dae3b9f26347f8a
muhhizbe <abumukhtar88@gmail.com>
2020-07-12 16:40:09 +0700

    update button in rightbar menu reputasi

--- resources/views/layouts/components/user/rightbar.blade.php
+++ resources/views/layouts/components/user/rightbar.blade.php
@@ -2,21 +2,21 @@
     <h5 class="text-left">Reputasi</h5>
     <hr>
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="#">
             <!-- <i class="fas fa-thumbs-up"></i> -->
             <i class="far fa-thumbs-up"></i>
             <span class="d-inline">21</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="#">
             <!-- <i class="fas fa-thumbs-down"></i> -->
             <i class="far fa-thumbs-down"></i>
             <span class="d-inline">12</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="#">
             <!-- <i class="fas fa-thumbs-up"></i> -->
             <i class="fas fa-egg"></i>
             <span class="d-inline">120</span>
